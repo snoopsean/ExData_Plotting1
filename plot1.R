81fbb876b98d828b36b2e2dcc7b71080745f08cc
snoopsean <snoopsean@zoho.com>
2014-06-05 11:55:54 -0700

    Had to reset the working directory and added some things to the plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,15 @@
-data<-read.table("./household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
-data[,"Date"]<-as.Date(data[,"Date"],format="%d/%m/%Y")
-mydata<-subset(data,Date=="2007-02-01" | Date=="2007-02-02")
+library(sqldf)
+zipUrl<-"http://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
+zipFile<-"household_power_consumption.zip"
+download.file(zipUrl,zipFile,method="auto")
+unzip(zipFile)
+
+textFile<-"household_power_consumption.txt"
+myData<-read.csv.sql(textFile,sql="select * from file where Date='1/2/2007' OR Date='2/2/2007'",header=TRUE,sep=";")
 png(file="plot1.png",width=480,height=480)
-hist(mydata$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+hist(myData$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
-read.csv.sql(r1,sql="select * from file where Date='16/12/2006'",sep=";")
+
+#data<-read.table("./household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
+#data[,"Date"]<-as.Date(data[,"Date"],format="%d/%m/%Y")
+#mydata<-subset(data,Date=="2007-02-01" | Date=="2007-02-02")
