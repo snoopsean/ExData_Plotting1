817e42f325a4727b322c3348f20d408e37a29256
snoopsean <snoopsean@zoho.com>
2014-06-05 12:05:03 -0700

    Final version of this plot1.R file

--- plot1.R
+++ plot1.R
@@ -1,11 +1,19 @@
-library(sqldf)
+#plot1.R
+
+#this section downloads the zip file and unzips it into the directory
 zipUrl<-"http://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
 zipFile<-"household_power_consumption.zip"
 download.file(zipUrl,zipFile,method="auto")
 unzip(zipFile)
 
+# this section reads data for only the 2 dates in questions and saves 
+# it as myData
 textFile<-"household_power_consumption.txt"
+library(sqldf)
 myData<-read.csv.sql(textFile,sql="select * from file where Date='1/2/2007' OR Date='2/2/2007'",header=TRUE,sep=";")
+
+# this section sets the file where the plot will be saved, runs the plot, the
+# closes the device
 png(file="plot1.png",width=480,height=480)
 hist(myData$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
