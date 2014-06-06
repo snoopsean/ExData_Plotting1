0c745d0ff71401cd39883c0fb14ab92bcdbf5ac1
snoopsean <snoopsean@zoho.com>
2014-06-05 20:21:41 -0700

    Done

--- plot1.R
+++ plot1.R
@@ -18,5 +18,3 @@ par(mfrow=c(1,1))
 png(file="plot1.png",width=480,height=480)
 hist(myData$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
-
-#data<-read.table("./household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
